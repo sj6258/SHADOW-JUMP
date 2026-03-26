214051b68af63efd008b75000b0742832c96ef33
Sj6258 <samriddhjoneja@gmail.com>
2026-03-26 23:24:30 +0530

    made death mechanics and collectibles

---
+++ scenes/killzone.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var timer = $Timer
+
+func _on_body_entered(_body):
+	print("death")
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()

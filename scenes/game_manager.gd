366e75464ee5fb740aec7770f23f30fe3fc207e6
Sj6258 <samriddhjoneja@gmail.com>
2026-05-16 14:27:20 +0530

    finished adding level 2

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -14,3 +14,15 @@ func add_score():
 	
 	score_label.text = "Total Coins : " + str(score) + " / 45"
 	score_label_2.text = "Total Coins : " + str(score) + "/ 20"
+
+
+
+
+
+
+func _on_main_menu_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_main_menu_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
